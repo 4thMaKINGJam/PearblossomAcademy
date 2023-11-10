83df1ee3c5e5d36577adc585897004a2427a2190
Yongmalyang <37902200+Yongmalyang@users.noreply.github.com>
2023-11-11 05:32:31 +0900

    player move&attack

---
+++ PearblossomAcademy/Assets/Script/PlayManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    public int playerLife;
+    public int monsterHP;
+
+    void Start()
+    {
+        
+    }
+
+}
