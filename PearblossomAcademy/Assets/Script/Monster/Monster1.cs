5f449858a662877d47f6660923d698d4bd1e9173
Karen <mingki080808@gmail.com>
2023-11-11 10:08:07 +0900

    foxcircle수정중

--- PearblossomAcademy/Assets/Script/Monster/Monster1.cs
+++ PearblossomAcademy/Assets/Script/Monster/Monster1.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Monster1 : MonoBehaviour
 {
     public float speed; //이동속도 = 6 정도로 세팅
-    public int playerBasicAttackDamage; //기본공격의 데미지량
+    public int monsterBasicAttackDamage; //기본공격의 데미지량
 
     public float basicAttackDelay; //기본공격 간격 조절
     private float curDelay; 
 
-    public GameObject playerBasicAttack; //기본공격 prefab
+    public GameObject monsterBasicAttack; //기본공격 prefab
     int dir = 1;
 
     Rigidbody2D monster1;
@@ -39,14 +39,20 @@ public class Monster1 : MonoBehaviour
         transform.position = curPos + movePos;
     }
 
+    //구미호 왕복 운동
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.tag == "Border")
         {
             dir *= (-1);
         }
 
-        }
+    }
 
+    //구미호가 player의 attack 받으면 damage 받게 하기
+
+
+
+    //구미호 여우구슬 공격 - 유도총알X
     void BasicAttack()
     {
         if (curDelay < basicAttackDelay)
